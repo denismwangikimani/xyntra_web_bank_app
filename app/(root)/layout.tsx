4e8c249f18e80b1533e77a51cb65384449f78975
Denis Mwangi <deniskimani2022@gmail.com>
2024-09-10 12:12:37 +0300

    added the sidebar and mobile nav bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,31 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //we will create a loggedIn state that will take in the users first and last name
+  const loggedIn = { firstName: "Denis", lastName: "Mwangi" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      {/* We will pass the loggedIn state to the Sidebar component */}
+      <Sidebar user={loggedIn} />
+      
+      {/* We will render the mobile nav(sidebar) */}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+// We will destructure the user prop from the Sidebar component
+const Sidebar = ({ user }: SiderbarProps) => {
+  // We will use the usePathname hook to get the current route to determine the active link if the route matches the link route
+  const pathname = usePathname();
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            width={34}
+            height={34}
+            src="/icons/logo.svg"
+            alt="xyntra logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Xyntra</h1>
+        </Link>
+
+        {/* We will map over the sidebarLinks, then return each item as a Link, by passing in the href and key */}
+        {sidebarLinks.map((link) => {
+          // We will check if the current route matches the link route "/", if it does we will add the active class
+          const isActive =
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              //we will use cn to conditionally add the active class
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  fill
+                  src={link.imgURL}
+                  alt={link.label}
+                  //we will turn up the brightness and invert it if the link is active
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+
+        USER
+      </nav>
+
+      FOOTER
+    </section>
+  );
+};
+
+export default Sidebar;
