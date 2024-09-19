f52c82623278a60aa2c9ee88daebbea4806c8681
Denis Mwangi <deniskimani2022@gmail.com>
2024-09-19 10:11:54 +0300

    allowed users to add multiple banks

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 // We will destructure the user prop from the Sidebar component
 const Sidebar = ({ user }: SiderbarProps) => {
@@ -57,7 +58,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
           );
         })}
 
-        USER
+        <PlaidLink user={user} />
       </nav>
 
       <Footer user={user} />
